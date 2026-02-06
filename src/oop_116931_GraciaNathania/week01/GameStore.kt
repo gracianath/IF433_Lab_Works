e0e91cad20e4856c6c302c0113a4b5bd1a5a3474
gracianath <gracianath66@gmail.com>
2026-02-06 16:19:45 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_116931_GraciaNathania/week01/GameStore.kt
+++ src/oop_116931_GraciaNathania/week01/GameStore.kt
@@ -2,5 +2,7 @@ package oop_116931_GraciaNathania.week01
 
 fun main() {
     val gameTitle: String = "MOBILE LEGEND"
-    val price: Int = 100
+    val price: Int = 100000
 }
+
+fun calculateDiscount(price: Double): Double = if (price > 500000) price * 0.8 else price * 0.9

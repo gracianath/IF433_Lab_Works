9787842eb62c1bb3451a2f64d9f4643d2e006576
gracianath <gracianath66@gmail.com>
2026-02-13 15:07:18 +0700

    week02: create basic Student class structure

--- src/oop_116931_GraciaNathania/week01/GameStore.kt
+++ src/oop_116931_GraciaNathania/week01/GameStore.kt
@@ -3,16 +3,17 @@ package oop_116931_GraciaNathania.week01
 fun main() {
     val gameTitle: String = "MOBILE LEGEND"
     val price: Int = 100000
-
+    val userName: String? = null
     val finalPrice = calculateDiscount(price)
 
-    printReceipt(title = gameTitle, price, finalPrice = finalPrice)
+    printReceipt(title = gameTitle, price, finalPrice = finalPrice, userName = userName)
 }
 
 fun calculateDiscount(price: Int): Int = if (price > 500000) (price * 0.8).toInt() else (price * 0.9).toInt()
 
-fun printReceipt(title: String, price: Int, finalPrice: Int) {
+fun printReceipt(title: String, price: Int, finalPrice: Int, userName: String?) {
     println("Nama Game: $title")
     println("Harga Awal: $price")
+    println("Nama Pengguna: ${userName ?: "Tidak ada catatan"}")
     println("Total Harga Akhir: $finalPrice")
 }
---
+++ src/oop_116931_GraciaNathania/week02/Student.kt
@@ -0,0 +1,9 @@
+package oop_116931_GraciaNathania.week02
+
+class Student(
+    val name: String,
+    val nim: String,
+    val major: String
+){
+    //body class
+}

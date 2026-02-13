0d6328adf3feae77b47b7410a931b3b1b90dae8d
gracianath <gracianath66@gmail.com>
2026-02-13 15:29:38 +0700

    week02: challenge refactor primary constructor using default args

--- src/oop_116931_GraciaNathania/week02/Student.kt
+++ src/oop_116931_GraciaNathania/week02/Student.kt
@@ -1,6 +1,6 @@
 package oop_116931_GraciaNathania.week02
 
-class Student(val name: String, val nim: String, val major: String){
+class Student(val name: String, val nim: String, val major: String, val gpa: Double = 0.0){
     constructor(name: String, nim: String) : this(name, nim, "Non-Matriculated"){
         println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
     }

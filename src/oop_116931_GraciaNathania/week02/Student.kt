0ec70c5a8c81c586f1653815ef70e13fafd46f02
gracianath <gracianath66@gmail.com>
2026-02-13 15:25:35 +0700

    week02: add secondary constructor

--- src/oop_116931_GraciaNathania/week02/Student.kt
+++ src/oop_116931_GraciaNathania/week02/Student.kt
@@ -1,6 +1,9 @@
 package oop_116931_GraciaNathania.week02
 
 class Student(val name: String, val nim: String, val major: String){
+    constructor(name: String, nim: String) : this(name, nim, "Non-Matriculated"){
+        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
+    }
     init{
         if (nim.length != 5){
             println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")

42ef04bd0a530a24876aa43fe75ed7de6f33cbb3
gracianath <gracianath66@gmail.com>
2026-02-13 16:07:28 +0700

    week02: (task 1) implement library input system

--- src/oop_116931_GraciaNathania/week02/Library.kt
+++ src/oop_116931_GraciaNathania/week02/Library.kt
@@ -1,6 +1,8 @@
 package oop_116931_GraciaNathania.week02
 
-class Loan(val bookTitle: String, val borrower: String, val bookAuthor: String, val bookPrice: String, var loanDuration: Int = 1){
+import java.util.Scanner
+
+class Loan(val bookTitle: String, val borrower: String, var loanDuration: Int = 1){
     fun calculateFine(): Int {
         return if (loanDuration > 3){
             (loanDuration - 3) * 2000
@@ -8,4 +10,34 @@ class Loan(val bookTitle: String, val borrower: String, val bookAuthor: String, 
             0
         }
     }
+}
+
+fun main(){
+    val scanner = Scanner(System.`in`)
+
+    println("--- LIBRARY SYSTEM ---")
+
+    print("Masukkan Judul: ")
+    val judul = scanner.nextLine()
+
+    print("Masukkan Peminjam: ")
+    val peminjam = scanner.nextLine()
+
+    print("Masukkan Lama Pinjam: ")
+    var lamaPinjam = scanner.nextInt()
+
+    scanner.nextLine() //bersihkan buffer
+
+    if(lamaPinjam < 0){
+        lamaPinjam = 1
+    }
+
+    val loan1 = Loan(judul, peminjam, lamaPinjam)
+
+    println("--- DETAIL PEMINJAMAN ---")
+    println("Judul Buku: ${loan1.bookTitle}")
+    println("Nama Peminjam: ${loan1.borrower}")
+    println("Lama Pinjam: ${loan1.loanDuration}")
+    println("Total Denda: ${loan1.calculateFine()}")
+
 }

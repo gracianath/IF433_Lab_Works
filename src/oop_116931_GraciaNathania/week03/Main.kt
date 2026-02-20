12b51dd1cb8d4d79a6051f3f4b8a4df157a3a035
gracianath <gracianath66@gmail.com>
2026-02-20 15:29:55 +0700

    week03: (task 1) implement Weapon testing in main

--- src/oop_116931_GraciaNathania/week03/Main.kt
+++ src/oop_116931_GraciaNathania/week03/Main.kt
@@ -10,6 +10,18 @@ fun main(){
     e.increasePerformance()
 
     println("Pajak yang harus dibayar: ${e.tax}")
+
+    val myWeapon = Weapon("Slayer Sword")
+
+    println("-- Test 1: Set Damage Negatif --")
+    myWeapon.damage = -50
+    println("Damage saat ini: ${myWeapon.damage}")
+
+    println("\n-- Test 2: Set Damage Overpower --")
+    myWeapon.damage = 9999
+    println("Damage saat ini: ${myWeapon.damage}")
+
+    println("\n-- Test 3: Cek Tier --")
+    println("Tier Senjata: ${myWeapon.tier}")
 }
 
-//selesai

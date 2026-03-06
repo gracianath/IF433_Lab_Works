6a800e566ef1e9d0fde6d9345bd7262673a2fb2c
gracianath <gracianath66@gmail.com>
2026-03-06 15:27:38 +0700

    week05: (task 2) implement payment polymorphism

--- src/oop_116931_GraciaNathania/week05/Main.kt
+++ src/oop_116931_GraciaNathania/week05/Main.kt
@@ -35,4 +35,14 @@ fun main(){
     val hitungLingkaran = MathHelper()
     println("Luas Lingkaran = ${hitungLingkaran.hitungLuasLingkaran(5.2)}")
 
+    println("=== TUGAS 2 ===")
+    val eWallet1 = EWallet("Budi", 50000.0)
+    val card1 = CreditCard(100000.0, 1000000.0, "Budi")
+
+    val payments: List<PaymentMethod> = listOf(eWallet1, card1)
+
+    for (payment in payments) {
+        println("--- Memproses pembayaran untuk: ${payment.accountName} ---")
+        payment.processPayment(75000.0)
+    }
 }

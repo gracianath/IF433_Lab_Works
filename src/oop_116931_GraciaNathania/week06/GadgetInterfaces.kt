a8ba83f16dcd082e060393029f2c3ac6f94a740b
gracianath <gracianath66@gmail.com>
2026-03-13 14:57:16 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_116931_GraciaNathania/week06/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package oop_116931_GraciaNathania.week06
+
+interface Camera{
+    fun turnOn(){ println("Lensa kamera terbuka dan sensor aktif.")}
+}
+
+interface Phone{
+    fun turnOn() { println("Sinyal seluler mencari jaringan.")}
+}

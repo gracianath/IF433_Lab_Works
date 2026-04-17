406b770a07aea0f3c584e2e10712fc3cbb308481
gracianath <gracianath66@gmail.com>
2026-04-17 15:17:31 +0700

    week08: test safe casting in main

--- src/latihan/ElectricCar.kt
+++
@@ -1,9 +0,0 @@
-package latihan
-
-import latihan.Car
-
-class ElectricCar (brand: String, numberofDoors: Int, val batteryCapacity: Int) : Car(brand, numberofDoors) {
-    final override fun accelerate() {
-        println("$brand berakselerasi dalam sunyi. Kapasitas baterai: $batteryCapacity%.")
-    }
-}
--- src/latihan/w5PaymentMethod.kt
+++
@@ -1,5 +0,0 @@
-package latihan
-
-abstract class w5PaymentMethod (val accountName: String) {
-    abstract fun processPayment(amount: Double): Double
-}

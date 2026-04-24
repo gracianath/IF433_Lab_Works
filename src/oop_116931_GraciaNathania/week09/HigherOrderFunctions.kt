235d1a324186073787dd48f9c495077394b58175
gracianath <gracianath66@gmail.com>
2026-04-24 09:16:51 +0700

    week09: implement sortedByDescending and forEach

--- src/oop_116931_GraciaNathania/week09/HigherOrderFunctions.kt
+++ src/oop_116931_GraciaNathania/week09/HigherOrderFunctions.kt
@@ -12,4 +12,13 @@ fun main(){
     val asString = multiplied.map { "Rp ${it}k" }
     println("Multiplied: $multiplied")
     println("Formatted: $asString")
+
+    println("\n=== HOF: SORTED & FOREACH ===")
+    val randomNames = listOf("Charlie", "Alice", "Bob")
+
+    val sortedReverse = randomNames.sortedByDescending { it }
+
+    sortedReverse.forEach { name ->
+        println("User: $name")
+    }
 }

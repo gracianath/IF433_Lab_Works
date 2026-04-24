92d955c3c2e1cae4e711416038533ec0d1ee30e0
gracianath <gracianath66@gmail.com>
2026-04-24 13:24:27 +0700

    week09: (task) print worst performers and unique pairs

--- src/oop_116931_GraciaNathania/week09/TradingDashboard.kt
+++ src/oop_116931_GraciaNathania/week09/TradingDashboard.kt
@@ -40,9 +40,10 @@ fun main(){
         .map { it.pair }
         .toSet()
 
-    println("=== CRYPTO TRADING DASHBOARD ===")
+    println("==== CRYPTO TRADING DASHBOARD ====")
     topPerformersString.forEach { println(it) }
 
     worstPerformersString.forEach { println(it) }
     println("Unique Pairs: $uniquePairs")
+
 }

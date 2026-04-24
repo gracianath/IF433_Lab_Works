85db41af294b0f13f6077012f9a23722853dd4de
gracianath <gracianath66@gmail.com>
2026-04-24 09:36:39 +0700

    week09: (task) create TradeLog data class

---
+++ src/oop_116931_GraciaNathania/week09/TradingModels.kt
@@ -0,0 +1,5 @@
+package oop_116931_GraciaNathania.week09
+
+import javax.swing.text.Position
+
+data class TradeLog(val pair: String, val position: String, val leverage: Int, val roe: Double, val status: String)

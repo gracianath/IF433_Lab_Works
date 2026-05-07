e2f1e9c28af11475709896dfdffee1f821589658
gracianath <gracianath66@gmail.com>
2026-05-07 23:04:02 +0700

    week10: (task) test full generic crypto architecture

--- src/oop_116931_GraciaNathania/week10/Main.kt
+++ src/oop_116931_GraciaNathania/week10/Main.kt
@@ -8,16 +8,16 @@ fun main() {
     println("Isi intBox: ${intBox.value}")
     println("Isi stringBox: ${stringBox.value}")
 
-    println("\n=== TEST MULTIPLE PARAMETERS ===")
+    println("\n==== TEST MULTIPLE PARAMETERS ====")
     val itemPrice = PairBox("Bitcoin", 65000)
     println("Aset: ${itemPrice.key}, Harga: ${itemPrice.value} USD")
 
-    println("\n=== TEST GENERIC FUNCTIONS ===")
+    println("\n==== TEST GENERIC FUNCTIONS ====")
     printData(3.14)
     val result = processData("Stable Coin")
     println("Hasil proses: $result")
 
-    println("\n=== TEST CONSTRAINTS ===")
+    println("\n==== TEST CONSTRAINTS ====")
     val math = MathBox(10.5, 20)
     println("Total: ${math.sum()}")
     println("Terbesar: ${getMax(45, 90)}")

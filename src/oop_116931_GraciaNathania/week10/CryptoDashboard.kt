6acad61b3cca9567486e20f316ac2a6b8fddd0b5
gracianath <gracianath66@gmail.com>
2026-05-07 22:38:59 +0700

    week10: (task) populate coin repository

--- src/oop_116931_GraciaNathania/week10/CryptoDashboard.kt
+++ src/oop_116931_GraciaNathania/week10/CryptoDashboard.kt
@@ -2,4 +2,8 @@ package oop_116931_GraciaNathania.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 1.25))
+    coinRepo.add(Coin("ETH", 10.50))
+    coinRepo.add(Coin("USDT", 5000.00))
 }

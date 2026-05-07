cbdc32dcf5761669512ccd8359a71f016b5776aa
gracianath <gracianath66@gmail.com>
2026-05-07 22:41:15 +0700

    week10: (task) wrap repository data into ApiResponse

--- src/oop_116931_GraciaNathania/week10/CryptoDashboard.kt
+++ src/oop_116931_GraciaNathania/week10/CryptoDashboard.kt
@@ -6,4 +6,6 @@ fun main() {
     coinRepo.add(Coin("BTC", 1.25))
     coinRepo.add(Coin("ETH", 10.50))
     coinRepo.add(Coin("USDT", 5000.00))
+
+    val response = ApiResponse("200 OK", coinRepo.getAll())
 }

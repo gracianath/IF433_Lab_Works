cac1509d9d3b2cd2905b40531d68ddaee47901bc
gracianath <gracianath66@gmail.com>
2026-05-08 15:10:12 +0700

    week11: (task) implement device search with let

--- src/oop_116931_GraciaNathania/week11/SmartHomeDashboard.kt
+++ src/oop_116931_GraciaNathania/week11/SmartHomeDashboard.kt
@@ -24,4 +24,10 @@ fun main(){
     homeDevices.add(acInverter)
 
     homeDevices.add(SmartDevice("Picolo's Auto Feeder", "Pet Care", true, 10))
+
+    val searchResult = homeDevices.find { it.category == "Camera"}
+    searchResult?.let {
+        println("Hasil Pencarian Kamera:")
+        println(it.diagnose())
+    }
 }

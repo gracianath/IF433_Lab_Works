0759d1d3c07308e9f816f9715b590c9d19d408b6
gracianath <gracianath66@gmail.com>
2026-05-08 15:15:12 +0700

    week11: (task) calculate total power usage

--- src/oop_116931_GraciaNathania/week11/SmartHomeDashboard.kt
+++ src/oop_116931_GraciaNathania/week11/SmartHomeDashboard.kt
@@ -34,4 +34,9 @@ fun main(){
     with(homeDevices) {
         println("\nTotal Perangkat Terdaftar: ${this.size}")
     }
+
+    val totalPower = homeDevices.run {
+        sumOf { it.powerLoad }
+    }
+    println("Total Konsumsi Daya: $totalPower Watt")
 }

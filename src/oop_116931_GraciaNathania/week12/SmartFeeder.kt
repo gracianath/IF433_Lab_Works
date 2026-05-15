9555d0fb200dd65b65e789f4d29f8da36f0d0cdf
gracianath <gracianath66@gmail.com>
2026-05-15 15:21:28 +0700

    week12: (task) handle feeding execution with multiple catch

--- src/oop_116931_GraciaNathania/week12/SmartFeeder.kt
+++ src/oop_116931_GraciaNathania/week12/SmartFeeder.kt
@@ -20,4 +20,14 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
 
 fun main() {
     var currentKibbleStock = 50
+
+    try {
+        currentKibbleStock = dispenseKibble(requestedGram = 80, availableGram = currentKibbleStock, isJammed = false)
+    } catch (e: DispenserJamException) {
+        println("ERROR DISPENSER: ${e.message}")
+    } catch (e: FoodEmptyException) {
+        println("ERROR STOK MAKANAN: ${e.message}")
+    } catch (e: Exception) {
+        println("ERROR UMUM: ${e.message}")
+    }
 }

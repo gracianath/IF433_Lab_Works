fd9ab92a3b15124cc5ae35a5d83974a41181566b
gracianath <gracianath66@gmail.com>
2026-05-15 15:28:50 +0700

    week12: (task) test full robust smart feeder pipeline

--- src/oop_116931_GraciaNathania/week12/SmartFeeder.kt
+++ src/oop_116931_GraciaNathania/week12/SmartFeeder.kt
@@ -40,6 +40,6 @@ fun main() {
         println("Makan sore sukses! Sisa stok kibble: $currentKibbleStock gr")
     }.onFailure { error ->
         println("Peringatan ke Pemilik: ${error.message}")
-        println("(Opsional: Berikan chicken jerky secara manual)")
+        println("(Opsional: Berikan chicken jerky secara manual).")
     }
 }
